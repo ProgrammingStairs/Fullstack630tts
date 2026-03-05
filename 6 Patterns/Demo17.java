ccb2db3783dbd17c59e2fdfd0a0fa0d76c4f7276
gauravKothari <kotharigaurav6@gmail.com>
2026-03-05 19:44:33 +0530

    java

---
+++ 6 Patterns/Demo17.java
@@ -0,0 +1,27 @@
+/*
+        1
+       10
+      101
+     1010
+    10101
+*/
+import java.util.Scanner;
+class Demo17{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows : ");
+        int rows = sc.nextInt();
+        for(int i=1 ; i<=rows ; i++){
+            for(int sp=1;sp<=rows-i;sp++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=i ; j++){
+                if(j%2==0)
+                    System.out.print("0");
+                else
+                    System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 6 Patterns/Demo18.java
@@ -0,0 +1,24 @@
+/*
+    12345
+     1234
+      123
+       12
+        1
+*/
+import java.util.Scanner;
+class Demo18{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows : ");
+        int rows = sc.nextInt();
+        for(int i=rows ; i>=1 ; i--){
+            for(int sp=1;sp<=rows-i;sp++){
+                System.out.print(" ");
+            }
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
