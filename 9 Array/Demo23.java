340d6494b36a5f87adcb62ad0dcb2b98fe0656d1
gauravKothari <kotharigaurav6@gmail.com>
2026-04-02 19:36:09 +0530

    java

---
+++ 9 Array/Demo23.java
@@ -0,0 +1,32 @@
+// Example showing the concept of 2d array
+import java.util.Scanner;
+class Demo23
+{
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter size : ");
+       int size = sc.nextInt();
+       int i,arr[] = new int[size];
+       Demo23 obj = new Demo23();
+       obj.getArrayElements(arr); 
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
